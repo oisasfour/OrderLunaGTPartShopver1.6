92c28dc1b3c8014093ff0422cb10a4c4412418de
oisasfour <oisascanvafour@gmail.com>
2026-04-10 22:01:32 +0700

    Update user credentials and version number

--- src/App.tsx
+++ src/App.tsx
@@ -90,8 +90,8 @@ export default function App() {
       type: "premium",
     },
 
-    "1234": {
-      password: "1234",
+    "9079774602": {
+      password: "101033",
       type: "premium",
     },
 
@@ -551,7 +551,7 @@ export default function App() {
             }}
           />
 
-          <h2 style={{ color: "#6d4c41" }}>LunarGT PartShop 1.6.00</h2>
+          <h2 style={{ color: "#6d4c41" }}>LunarGT PartShop 1.6.0</h2>
         </div>
 
         <button
