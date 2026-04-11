8d8e22283ad4280cbfd30ad97ccf4815f3704889
oisasfour <oisascanvafour@gmail.com>
2026-04-12 04:14:25 +0700

    Update version number to 1.6.1 in App component

--- src/App.tsx
+++ src/App.tsx
@@ -551,7 +551,7 @@ export default function App() {
             }}
           />
 
-          <h2 style={{ color: "#6d4c41" }}>LunarGT PartShop 1.6.0</h2>
+          <h2 style={{ color: "#6d4c41" }}>LunarGT PartShop 1.6.1</h2>
         </div>
 
         <button
